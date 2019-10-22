53593a99afb1a4a4d7c2ba7ceee552ae481f8102
JlacTo4Ka <56308615+JlacTo4Ka@users.noreply.github.com>
2019-10-22 17:22:36 +0200

    2

---
+++ задание2.py
@@ -0,0 +1,6 @@
+from PhyConstant import g, k, e, h
+from math import cos, tan, sqrt, pi
+v = sqrt((g*100*1/tan(b)**2)/(2*cos(a)**2*(1 - 1/tan(b)*1/tan(a))))
+print(v)
+N = 2/sqrt(pi)*h/(k*200)**(3/2)*e**(-300/k*200)*300**(200/2)
+print(N)
