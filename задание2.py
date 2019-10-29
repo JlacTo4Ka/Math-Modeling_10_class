7ae09babe4a247ec66631216e2c4765ad633fff6
JlacTo4Ka <56308615+JlacTo4Ka@users.noreply.github.com>
2019-10-29 17:02:19 +0200

    лаба4

--- задание2.py
+++ задание2.py
@@ -1,6 +1,5 @@
-from PhyConstant import g, k, e, h
-from math import cos, tan, sqrt, pi
-v = sqrt((g*100*1/tan(b)**2)/(2*cos(a)**2*(1 - 1/tan(b)*1/tan(a))))
-print(v)
-N = 2/sqrt(pi)*h/(k*200)**(3/2)*e**(-300/k*200)*300**(200/2)
-print(N)
+import numpy as np
+def MASSSSSIV(a):
+    print(np.prod(a))
+MASSSSSIV(np.arange(1,10,1))
+    
