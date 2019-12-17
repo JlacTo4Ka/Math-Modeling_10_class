9d85646a6225b40a81d2f9a96886f7280497c62e
JlacTo4Ka <56308615+JlacTo4Ka@users.noreply.github.com>
2019-12-17 17:12:10 +0200

    lab10

---
+++ 222222222(2).py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt 
+
+def diff(z,t):
+    s,v = z
+    ds_dt = v
+    dv_dt = -9.8*np.sin(s/l)-(k/a)*v
+    return ds_dt , dv_dt 
+
+s0 = 1.5
+k = 6
+a = 13
+v0 = 1
+l=1
+z0 = s0, v0
+
+solve =odeint(diff,z0,t)
+
+plt.plot(solve[:,0],'g', label='Grafiki')
+plt.legend
+plt.show()
